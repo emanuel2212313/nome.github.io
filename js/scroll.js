fd5b2c82b41a2765474a46c0380251e64a12dc8d
Emanuel <arcanjoemanuel10@gmail.com>
2026-01-17 11:14:38 -0300

    Adicionando ao gthub

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const elementVisible = 120;
+
+    if (elementTop < windowHeight - elementVisible) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
